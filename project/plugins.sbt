8e4820d4f373c1d662e58937a8dba9169981c863
Cristian Spinetta <cebs91@gmail.com>
2016-09-21 14:30:00 -0300

    add script to deploy on amazon.

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,8 @@
 resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
 
+// The Typesafe repository
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
 
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
@@ -7,3 +10,5 @@ addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.3")
+
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.3")
---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "checking if I'm standing in project's root"
+cat navigation-api/version.sbt
+if [ $? -eq 0 ]; then
+    echo "I am, checking if repository has changes"
+    if git diff-index --quiet HEAD --; then
+        echo "executing build... "
+        sbt clean test navigation-api/assembly
+        echo "done. starting deploy!"
+        scp -i ~/.ssh/easy-travel-amazon.pem navigation-api/target/scala-2.11/footpath-routing-api.jar ubuntu@ec2-54-187-65-126.us-west-2.compute.amazonaws.com:~/app/
+        ssh -i ~/.ssh/easy-travel-amazon.pem ubuntu@ec2-54-187-65-126.us-west-2.compute.amazonaws.com "~/app/scripts/shutdown && ~/app/scripts/startup"
+        echo "all done. merry christmas! Miameeeeeeeeeeeeeeeeee!"
+    else
+        echo "project has uncommited changes! commit them to continue, I'm guessing you don't want to deploy unversioned changes... or do you? D:"
+    fi
+else
+    echo "please, cd to project's root directory to execute this script. thank you very much for your patience."
+fi
