849a6814a725e6f6e9334069e13144669d9b4b3f
Mariano Sanchez de Vera Bastone <mssanchez@despegar.com>
2016-09-27 18:29:45 -0300

    Ramps to vector in api

--- navigation-api/src/main/scala/module/MapModule.scala
+++ navigation-api/src/main/scala/module/MapModule.scala
@@ -36,7 +36,7 @@ trait MapModule extends GraphSupport with LazyLoggerSupport with ApiEnvConfig {
   }
 
   def ramps(coordinate: Coordinate, radius: Double): Try[Vector[Ramp]] = Try {
-    GeoSearch.findNearestByRadius(coordinate, radius, RampRepository.findAll, (ramp: Ramp) ⇒ Seq(ramp.coordinate))
+    GeoSearch.findNearestByRadius(coordinate, radius, RampRepository.findAll.toVector, (ramp: Ramp) ⇒ Seq(ramp.coordinate))
   }
 
 }
