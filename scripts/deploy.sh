03ea525cc9872a558d440ff7a4ee0e31755c5f8f
Cristian Spinetta <cebs91@gmail.com>
2016-11-19 12:58:39 -0300

    := update prod webserver host

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,8 @@ if [ $? -eq 0 ]; then
         echo "executing build... "
         sbt clean test navigation-api/assembly
         echo "done. starting deploy!"
-        scp -i ~/.ssh/easy-travel-amazon.pem navigation-api/target/scala-2.11/footpath-routing-api.jar ubuntu@ec2-54-187-65-126.us-west-2.compute.amazonaws.com:~/app/
-        ssh -i ~/.ssh/easy-travel-amazon.pem ubuntu@ec2-54-187-65-126.us-west-2.compute.amazonaws.com "~/app/scripts/shutdown && ~/app/scripts/startup"
+        scp -i ~/.ssh/easy-travel-amazon.pem navigation-api/target/scala-2.11/footpath-routing-api.jar ubuntu@ec2-54-187-107-69.us-west-2.compute.amazonaws.com:~/app/
+        ssh -i ~/.ssh/easy-travel-amazon.pem ubuntu@ec2-54-187-107-69.us-west-2.compute.amazonaws.com "~/app/scripts/shutdown && ~/app/scripts/startup"
         echo "all done. merry christmas! Maiameeeeeeeeeeeeeeeeee!"
 #    else
 #        echo "project has uncommited changes! commit them to continue, I'm guessing you don't want to deploy unversioned changes... or do you? D:"
